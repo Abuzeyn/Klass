936618f6cb73536736f41e89a5f054fbf6ac9dd7
User <User@DESKTOP-ILM46JN>
2023-04-08 10:15:50 +0400

    Add project files.

---
+++ Klass/Bank.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klass
+{
+    internal class Bank
+    {
+        public static string ReadFromConsoleName()
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            return name;
+        }
+        public static int ReadFromConsoleAge()
+        {
+            Console.WriteLine("Введите ваш возраст");
+            int age = Convert.ToInt32(Console.ReadLine());
+            return age;
+
+
+        }
+        public static int ReadFromConsoleProductCount()
+        {
+            Console.WriteLine("Введите количество продуктов");
+            int productCount = Convert.ToInt32(Console.ReadLine());
+            return productCount;
+        }
+        public static int ReadFromConsoleSumMoney()
+        {
+            Console.WriteLine("Введите сумму средств");
+            int sumMoney = Convert.ToInt32(Console.ReadLine());
+            return sumMoney;
+
+        }
+        public static void AnswerPromotion(int age, int productCount, int sumMoney) 
+        {
+        if (age>60||sumMoney>1000000)
+            {
+                Console.WriteLine($"{"Поздравляем!"} {"Вы учавствуете в акции"}");
+            }
+        else if(productCount>=3&&sumMoney>400000)
+            {
+                Console.WriteLine($"{"Поздравляем!"} {"Вы учавствуете в акции"}");
+
+            }
+        else
+            {
+                Console.WriteLine($" {"Для вас нет никаких акций"}");
+            }
+        }
+    }
+}
---
+++ Klass/Program.cs
@@ -0,0 +1,6 @@
+﻿using Klass;
+Bank.ReadFromConsoleName();
+int a=Bank.ReadFromConsoleAge();
+int b=Bank.ReadFromConsoleProductCount();
+int c=Bank.ReadFromConsoleSumMoney();
+Bank.AnswerPromotion(a, b, c);
